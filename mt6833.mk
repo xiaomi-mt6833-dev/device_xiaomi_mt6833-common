5bcacf8b5d5a6af6b02eabab1169c4160ccde6f3
Tim Zimmermann <tim@linux4.de>
2022-08-26 14:29:14 +0200

    mt6833-common: Initial health AIDL implementation
    * Granted correct permissions on fb0 by default
    * Enabled suspend according to
      https://android.googlesource.com/platform/hardware/interfaces/+/refs/tags/android-13.0.0_r3/health/aidl#charger-enable-suspend
    
    Change-Id: I41b84840a03a57d46beb1a7520182de2bb29d2f5
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- mt6833.mk
+++ mt6833.mk
@@ -165,9 +165,8 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-service \
-    android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-impl.recovery
+    android.hardware.health-service.mt6833 \
+    android.hardware.health-service.mt6833-recovery
 
 # IMS
 PRODUCT_BOOT_JARS += \
