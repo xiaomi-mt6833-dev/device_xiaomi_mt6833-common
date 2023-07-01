d95ad2315bd15bc026dca06bc2f1a909c0f0b8ab
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-07-01 17:26:41 +0700

    mt6833-common: Enforce RRO for all overlays
    Change-Id: Iceb177c9b5322354b8b9f9ce81a91dc04a2350a8
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- mt6833.mk
+++ mt6833.mk
@@ -202,6 +202,9 @@ PRODUCT_PACKAGES += \
     android.hardware.neuralnetworks@1.3.vendor \
     libtextclassifier_hash.vendor
 
+# Overlays
+PRODUCT_ENFORCE_RRO_TARGETS := *
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
