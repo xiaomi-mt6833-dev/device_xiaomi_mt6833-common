3d040f667e9dce0d5dc8e93fa75ba2d7820f78c8
Lokesh Gidra <lokeshgidra@google.com>
2023-07-19 20:19:00 +0100

    mt6833-common: Inherit the Dalvik heap configuration for a 6GB device
     * We have multiple variants with varying RAM capacities but
       it's not worth making custom values for each variant,
       can end up doing more harm than good.
    
     * For now lets just use the verified ones from AOSP for the
       highest capacity variant they have.
    
    Change-Id: Ib6b1d7ad933fe3867dc6253e229a3c6470135e75
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- mt6833.mk
+++ mt6833.mk
@@ -7,6 +7,9 @@
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+# Dalvik VM Configuration
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+
 # Enable Virtual A/B OTA
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch.mk)
 
