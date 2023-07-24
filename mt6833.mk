0969a9c00e7438be5204c584abea5f7cfcd0c5e5
bengris32 <bengris32@protonmail.ch>
2023-07-24 10:22:51 +0100

    mt6833-common: Configure vendor logtags
    Change-Id: I83b1e3d5691c7c22a5fef5f03ce3172ac47298d3
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- mt6833.mk
+++ mt6833.mk
@@ -258,6 +258,9 @@ PRODUCT_COPY_FILES += \
     system/core/libprocessgroup/profiles/cgroups_30.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
     $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
+# Properties
+include $(LOCAL_PATH)/configs/props/vendor_logtag.mk
+
 # Radio
 PRODUCT_PACKAGES += \
     android.hardware.radio.config@1.3.vendor \
