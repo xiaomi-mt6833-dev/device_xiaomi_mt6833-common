e83eadd66e7606e5e8bc32d01c31774599644db7
Sushrut1101 <guptasushrut@gmail.com>
2023-08-23 23:26:04 +0530

    mt6833-common: Configure Fingerprint stack
    Change-Id: I5ab15dae7339eb00a3198c55b0c5653982c3e0cf
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- mt6833.mk
+++ mt6833.mk
@@ -69,6 +69,11 @@ PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.1-impl-mock \
     fastbootd
 
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.3-service.xiaomi \
+    vendor.xiaomi.hardware.fingerprintextension@1.0.vendor
+
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-service \
