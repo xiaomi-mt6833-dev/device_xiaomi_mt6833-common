c6d17e1dba21c15be7fdc342c36db0a260f17494
Sushrut1101 <guptasushrut@gmail.com>
2023-08-23 14:54:37 +0530

    mt6833-common: Configure common Thermal stack
    Change-Id: Ic38b6805aff8fa732c40a048203d3ade6c4b3bd9
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- mt6833.mk
+++ mt6833.mk
@@ -161,3 +161,8 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/mediatek \
     hardware/xiaomi
+
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@2.0.vendor
