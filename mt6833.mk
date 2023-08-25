7a51c96a44d553a4e8c44f0d9c551a30967c1fb4
Sushrut1101 <guptasushrut@gmail.com>
2023-08-25 21:47:50 +0530

    mt6833-common: Configure common Camera stack
    Change-Id: Ia6269a01dea26200f6000057ede7d23b7069b347
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- mt6833.mk
+++ mt6833.mk
@@ -57,6 +57,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.cas@1.2-service-lazy
 
+# Camera
+PRODUCT_PACKAGES += \
+    android.hardware.camera.device@3.6.vendor:64 \
+    android.hardware.camera.provider@2.6.vendor:64
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4-service.clearkey
