326c5b5c500ae2d52d7d77c3ea8b004095db5f5b
Sushrut1101 <guptasushrut@gmail.com>
2023-09-02 23:58:33 +0530

    mt6833-common: Configure Bluetooth stack
    Change-Id: I53fa562a01dcbd89e1778f44936708d49c5a524f
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- mt6833.mk
+++ mt6833.mk
@@ -79,6 +79,15 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration_7_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth.audio@2.1-impl \
+    android.hardware.bluetooth@1.1.vendor
+
+PRODUCT_PACKAGES += \
+    audio.bluetooth.default \
+    libbluetooth_audio_session
+
 # Boot Control
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.2-service \
