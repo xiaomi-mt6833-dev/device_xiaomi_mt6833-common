6bacdd9acb426e11dc9b0c2aa49a303088e1f505
Sushrut1101 <guptasushrut@gmail.com>
2023-09-21 23:26:45 +0530

    mt6833-common: Symlink `audio_param` to `audio_param_global`
    * Our Audio HAL needs both /vendor/etc/audio_param and /vendor/etc/audio_param_global.
      Since both of these contain the same blobs here, let's create a
      symlink of /vendor/etc/audio_param to /vendor/etc/audio_param_global.
    
    * This fixes our Audio HAL crashes
    
    Change-Id: Ided8ddf9cefc2b219c1f3fc3c3342e0673b59487
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- Android.mk
+++ Android.mk
@@ -11,4 +11,15 @@ ifneq ($(filter camellia everpal light,$(TARGET_DEVICE)),)
 subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
 $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
 
+VENDOR_SYMLINKS := \
+	$(TARGET_OUT_VENDOR)/etc
+
+$(VENDOR_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	$(hide) echo "Making vendor symlinks"
+	@mkdir -p $(TARGET_OUT_VENDOR)/etc
+	@ln -sf /vendor/etc/audio_param $(TARGET_OUT_VENDOR)/etc/audio_param_global
+	$(hide) touch $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(VENDOR_SYMLINKS)
+
 endif
