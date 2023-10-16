8496c979a749adc95a1e38a7e212fdfadf9f6a29
Sushrut1101 <guptasushrut@gmail.com>
2023-10-16 17:50:49 +0530

    mt6833-common: Switch to AIDL Thermal service
    Change-Id: Ie8856ca460022b909f67513df4ff46679ab14952
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- mt6833.mk
+++ mt6833.mk
@@ -365,12 +365,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/google/interfaces \
+    hardware/google/pixel \
     hardware/mediatek \
     hardware/xiaomi
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.mediatek
+    android.hardware.thermal-service.mediatek
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
