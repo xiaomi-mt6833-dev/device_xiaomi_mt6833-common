e955d2011e7ef571e69ba92d38ab74097ef34716
Sushrut1101 <guptasushrut@gmail.com>
2023-10-20 12:51:49 +0530

    mt6833-common: Build MediaTek In-Call Service
    Change-Id: Iaea955f946d6283c0d147094c9007c7c3b867ce9
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- mt6833.mk
+++ mt6833.mk
@@ -65,6 +65,9 @@ PRODUCT_PACKAGES += \
     libdynproc \
     libhapticgenerator
 
+PRODUCT_PACKAGES += \
+    MtkInCallService
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_device.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_device.xml \
     $(LOCAL_PATH)/configs/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
