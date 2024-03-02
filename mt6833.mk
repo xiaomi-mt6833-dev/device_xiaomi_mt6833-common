0c7509a599de6e42b5264a5b2438745475a83dbe
Sushrut1101 <guptasushrut@gmail.com>
2024-03-02 21:53:31 +0530

    mt6833-common: Source build Fingerprint extension interface
    Change-Id: I0587d117fdc4bf79a0638e19e3842fd1b94c89a1
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- mt6833.mk
+++ mt6833.mk
@@ -128,6 +128,7 @@ PRODUCT_PACKAGES += \
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.xiaomi \
+    com.fingerprints.extension@1.0.vendor \
     vendor.xiaomi.hardware.fingerprintextension@1.0.vendor
 
 PRODUCT_COPY_FILES += \
