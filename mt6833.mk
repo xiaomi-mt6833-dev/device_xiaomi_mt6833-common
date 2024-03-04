8b400cc294b374bc0366e467bbdac1ac4f2bb9e9
Sushrut1101 <guptasushrut@gmail.com>
2024-03-04 18:10:12 +0530

    mt6833-common: Copy HotwordEnrollment permissions to correct paths
    Change-Id: Ib98c55213eef7f0b221a465837fa8e5b4c53f216
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- mt6833.mk
+++ mt6833.mk
@@ -173,8 +173,8 @@ PRODUCT_PACKAGES += \
 
 # Hotword Enrollment
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/permissions/com.android.hotwordenrollment.common.util.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/com.android.hotwordenrollment.common.util.xml \
-    $(LOCAL_PATH)/configs/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-hotword.xml
+    $(LOCAL_PATH)/configs/permissions/com.android.hotwordenrollment.common.util.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/com.android.hotwordenrollment.common.util.xml \
+    $(LOCAL_PATH)/configs/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
 
 # IMS
 PRODUCT_BOOT_JARS += \
