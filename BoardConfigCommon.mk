57a5894f30685edd56db051e7354f28823833694
Sushrut1101 <guptasushrut@gmail.com>
2024-03-09 18:59:11 +0530

    mt6833-common: Set SELinux to Enforcing
    This reverts commit 60cd03f.
    
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -44,7 +44,6 @@ TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a55
 # Boot Image
 BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2
 BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
 BOARD_BOOTIMG_HEADER_VERSION := 2
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
