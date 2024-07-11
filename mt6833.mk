003e847707e672f0ceef076718a84c7297fa3439
Aaron Kling <webgeek1234@gmail.com>
2024-07-11 21:27:51 +0100

    mt6833-common: Remove software omx codec references
    These are no longer available as of Android 14 QPR3. The C2 codecs
    available by default in the swcodec apex will be used instead.
    
    Change-Id: I8cdf3e700823779a7382c66c3df8410d41209b7c
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- mt6833.mk
+++ mt6833.mk
@@ -232,10 +232,6 @@ PRODUCT_PACKAGES += \
     libstagefright_foundation-v33
 
 PRODUCT_COPY_FILES += \
-    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
-
-PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2.xml \
     $(LOCAL_PATH)/configs/media/media_codecs_mediatek_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_mediatek_audio.xml \
     $(LOCAL_PATH)/configs/media/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
