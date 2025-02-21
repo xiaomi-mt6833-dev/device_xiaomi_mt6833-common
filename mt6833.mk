0fb72589d135205f95ab08ef094a4ee4e0329d24
LuK1337 <priv.luk@gmail.com>
2025-02-21 09:01:04 +0100

    mt6833-common: Switch to common lights HAL
    Change-Id: If515e7c3f6e5b6895076f1d8381fb2c22bd1b3de
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- mt6833.mk
+++ mt6833.mk
@@ -216,7 +216,7 @@ PRODUCT_PACKAGES += \
 
 # Light
 PRODUCT_PACKAGES += \
-    android.hardware.light-service.xiaomi
+    android.hardware.light-service.lineage
 
 # Linker
 PRODUCT_COPY_FILES += \
