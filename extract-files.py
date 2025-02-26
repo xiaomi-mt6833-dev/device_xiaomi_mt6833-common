04c66ee9119a1bc80cbbd0bdd1e81b7d5b0e3bce
Sushrut1101 <guptasushrut@gmail.com>
2025-02-27 00:42:08 +0530

    mt6833-common: Migrate to py extract-files
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

---
+++ extract-files.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.file import File
+from extract_utils.fixups_blob import (
+    BlobFixupCtx,
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.fixups_lib import (
+    lib_fixup_remove,
+    lib_fixups,
+    lib_fixups_user_type,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+from extract_utils.tools import (
+    llvm_objdump_path,
+)
+from extract_utils.utils import (
+    run_cmd,
+)
+
+namespace_imports = [
+    'device/xiaomi/mt6833-common',
+    'hardware/mediatek',
+    'hardware/xiaomi',
+]
+
+def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
+    return f'{lib}_{partition}' if partition == 'vendor' else None
+
+lib_fixups: lib_fixups_user_type = {
+    **lib_fixups,
+    ('vendor.mediatek.hardware.videotelephony@1.0',): lib_fixup_vendor_suffix,
+    ('libsink',): lib_fixup_remove,
+}
+
+def blob_fixup_return_1(
+    ctx: BlobFixupCtx,
+    file: File,
+    file_path: str,
+    symbol: str,
+    *args,
+    **kwargs,
+):
+    for line in run_cmd(
+        [
+            llvm_objdump_path,
+            '--dynamic-syms',
+            file_path,
+        ]
+    ).splitlines():
+        if line.endswith(f' {symbol}'):
+            offset, _ = line.split(maxsplit=1)
+
+            with open(file_path, 'rb+') as f:
+                f.seek(int(offset, 16))
+                f.write(b'\x01\x00\xa0\xe3')  # mov r0, #1
+                f.write(b'\x1e\xff\x2f\xe1')  # bx lr
+
+            break
+
+
+blob_fixups: blob_fixups_user_type = {
+    'system_ext/lib64/libsink.so': blob_fixup()
+        .add_needed('libshim_sink.so'),
+    'system_ext/lib64/libsource.so': blob_fixup()
+        .add_needed('libui_shim.so'),
+    ('vendor/bin/hw/android.hardware.gnss-service.mediatek', 'vendor/lib64/hw/android.hardware.gnss-impl-mediatek.so'): blob_fixup()
+        .replace_needed('android.hardware.gnss-V1-ndk_platform.so', 'android.hardware.gnss-V1-ndk.so'),
+    ('vendor/bin/hw/android.hardware.media.c2@1.2-mediatek','vendor/bin/hw/android.hardware.media.c2@1.2-mediatek-64b'): blob_fixup()
+        .add_needed('libstagefright_foundation-v33.so')
+        .replace_needed('libavservices_minijail_vendor.so', 'libavservices_minijail.so'),
+    'vendor/bin/hw/vendor.mediatek.hardware.mtkpower@1.0-service': blob_fixup()
+        .replace_needed('android.hardware.power-V2-ndk_platform.so', "android.hardware.power-V2-ndk.so"),
+    ('vendor/bin/mnld', 'vendor/lib64/libaalservice.so', 'vendor/lib64/libcam.utils.sensorprovider.so'): blob_fixup()
+        .add_needed('libshim_sensors.so'),
+    'vendor/etc/init/android.hardware.media.c2@1.2-mediatek-64b.rc': blob_fixup()
+        .regex_replace('mediatek', 'mediatek-64b'),
+    'vendor/etc/init/android.hardware.neuralnetworks@1.3-service-mtk-neuron.rc': blob_fixup() 
+        .regex_replace('start', 'enable'),
+    'vendor/etc/init/vendor.mediatek.hardware.mtkpower@1.0-service.rc': blob_fixup()
+        .regex_replace('audio', 'audio input'),
+    'vendor/etc/vintf/manifest/manifest_media_c2_V1_2_default.xml': blob_fixup()
+        .regex_replace('1.1', '1.2'),
+    ('vendor/lib64/hw/android.hardware.camera.provider@2.6-impl-mediatek.so','vendor/lib64/libmtkcam_stdutils.so'): blob_fixup()
+        .replace_needed('libutils.so', 'libutils-v32.so'),
+    'vendor/lib64/hw/vendor.mediatek.hardware.pq@2.15-impl.so': blob_fixup()
+        .replace_needed('libutils.so', 'libutils-v32.so'),
+    ('vendor/lib64/libwvhidl.so', 'vendor/lib64/mediadrm/libwvdrmengine.so'): blob_fixup()
+        .replace_needed('libprotobuf-cpp-lite-3.9.1.so', 'libprotobuf-cpp-full-3.9.1.so')
+}  # fmt: skip
+
+module = ExtractUtilsModule(
+    'mt6833-common',
+    'xiaomi',
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device(module)
+    utils.run()
