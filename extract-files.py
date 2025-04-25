3c75421e3394a56043f174c68dc7a4787fa28536
Michael Bestas <mkbestas@lineageos.org>
2025-04-25 14:30:45 +0200

    mt6833-common: Shim NVRAM/Radio blobs with libbase_shim
    See: https://cs.android.com/android/_/android/platform/system/libbase/+/46fc805282265715266c256fbe34cf1a886c475f
    
    Change-Id: I5127020e18114a13515d9dc382c95d689634243d
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -78,6 +78,8 @@ blob_fixups: blob_fixups_user_type = {
     ('vendor/bin/hw/android.hardware.media.c2@1.2-mediatek','vendor/bin/hw/android.hardware.media.c2@1.2-mediatek-64b'): blob_fixup()
         .add_needed('libstagefright_foundation-v33.so')
         .replace_needed('libavservices_minijail_vendor.so', 'libavservices_minijail.so'),
+    ('vendor/bin/hw/android.hardware.neuralnetworks@1.3-service-mtk-neuron', 'vendor/lib/libnvram.so', 'vendor/lib64/libnvram.so', 'vendor/lib64/libsysenv.so') : blob_fixup()
+         .add_needed('libbase_shim.so'),
     'vendor/bin/hw/vendor.mediatek.hardware.mtkpower@1.0-service': blob_fixup()
         .replace_needed('android.hardware.power-V2-ndk_platform.so', "android.hardware.power-V2-ndk.so"),
     ('vendor/bin/mnld', 'vendor/lib64/libaalservice.so', 'vendor/lib64/libcam.utils.sensorprovider.so'): blob_fixup()
